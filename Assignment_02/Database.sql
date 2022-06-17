2bb886472128dd8346715695161dd2e5c4cca512
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2022-06-17 14:23:14 +0700

    Update Database.sql
    update the table uses index and not use index

--- Assignment_02/Database.sql
+++ Assignment_02/Database.sql
@@ -1,6 +1,40 @@
-SELECT * FROM HumanResources.Employee
--- khong co index
-SELECT NationalIDNumber from HumanResources.Employee where VacationHours = 99
--- co index
-SELECT NationalIDNumber from HumanResources.Employee WHERE BusinessEntityID = 1
+-- index in HumnaResources.Employee
+SELECT * INTO NewEmployee 
+FROM HumanResources.Employee
 
+SELECT * INTO NewEmployeeIndex 
+FROM HumanResources.Employee
+
+CREATE INDEX NewIndex
+ON NewEmployeeIndex(VacationHours)
+
+DROP INDEX NewIndex ON NewEmployeeIndex
+
+SELECT *
+FROM NewEmployee 
+WHERE VacationHours=34
+
+SELECT * 
+FROM NewEmployeeIndex 
+WHERE VacationHours=34
+
+-- index in Person.Address
+SELECT * INTO Address_NoIndex
+FROM Person.Address
+
+SELECT * INTO Address_Index
+FROM Person.Address
+
+CREATE INDEX NewIndex
+ON Address_Index(City)
+
+set statistics	io on
+SELECT *
+FROM Address_NoIndex 
+WHERE City = 'Bothell'
+
+
+set statistics io on
+SELECT *
+FROM Address_Index
+WHERE City = 'Bothell'
